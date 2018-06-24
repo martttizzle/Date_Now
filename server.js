a70dd70bef827ff1c97854b80083463ddce4fcef
jealob <jealob@gmail.com>
2018-06-24 09:25:40 -0500

    updates

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================

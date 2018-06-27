43aba50b9effd52385ef61319a4c536523c14f6c
Saad1987 <saad.mourtada@gmail.com>
2018-06-27 14:00:44 -0500

    my branch changes

--- controllers/googlemaps.js
+++ controllers/googlemaps.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 
-module.exports = function () {
+module.exports = function (zipCode,type,miles) {
+
+    var meters = miles*(1/0.00062137119223733);
     var googleMapsClient = require('@google/maps').createClient({
         // You need to create an env variable GOOGLE_KEY and put your google api key there
-        key: process.env.GOOGLE_KEY
+        key: 'AIzaSyBAhNxc8BbsIMC5tFTNUSADF8vhSiNxXmA'
+        //process.env.GOOGLE_KEY
+        
     });
 
     googleMapsClient.geocode({
-        address: '55414'
+        address: zipCode
     }, function (err, response) {
         if (!err) {
             let location = response.json.results[0].geometry.location;
             // Geocode an address.
             googleMapsClient.placesNearby({
                 location: location,
-                radius: 1000,
-                type: 'restaurant'
+                radius: meters,
+                type: type
             }, function (err, response) {
                 if (!err) {
                     // console.log(response.json.results);

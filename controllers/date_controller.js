32429ca13e7e7c286e0b28df41834d9b114c562f
jealob <jealob@gmail.com>
2018-06-30 23:15:46 -0500

    Updates changes

--- controllers/date_controller.js
+++ controllers/date_controller.js
@@ -46,12 +46,10 @@ function getPopularity(data) {
     input.push(data[i].apiId);
   }
   Datenow.findAll({ where: { apiId: input } }).then(function (dbDateNow) {
-    // Perform a callback
-    console.log(data.length, dbDateNow.length);
     for (i in data) {
       let idFound = dbDateNow.find(search => search.apiId === data[i].apiId);
       (idFound) ? data[i].popularity = idFound.popularity : data[i].popularity = 0;
-      console.log("request", updatedData[i]);
+
     }
   });
   return updatedData

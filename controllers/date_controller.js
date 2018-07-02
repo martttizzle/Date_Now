5ead18b7397c71899b17f15881deb3ad30244d34
Saad1987 <saad.mourtada@gmail.com>
2018-07-01 21:27:38 -0500

    copy of master branch

--- controllers/date_controller.js
+++ controllers/date_controller.js
@@ -93,6 +93,8 @@ function getData(rawData) {
     place.googleRating = rawData[i].rating;
     place.pricing = rawData[i].price_level;
     place.address = rawData[i].vicinity;
+    //Added photo_reference for Martel
+    place.photo= rawData[i].photos[0].photo_reference
     formattedData.push(place);
   }
   return formattedData;
